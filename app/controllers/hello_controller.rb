1bc0b4e1ad01fc836e14499b61594ba690dd45ed
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-07-22 09:42:39 -0400

    got test to pass

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,7 @@
+class HelloController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def hello_world
+  	render :hello_world
+  end
+end
